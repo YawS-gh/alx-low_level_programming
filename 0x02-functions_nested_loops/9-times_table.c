d7186382d87530c34470077de5ac5c8d856c3d1a
Yaw Sarkodie <yaw_sarkodie2003@yahoo.com>
2023-04-01 08:21:06 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: 0
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+	int m;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+			if (k > 9)
+			{
+				l = k % 10;
+				m = (l - k) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(l + '0');
+				_putchar(m + '0');
+			}
+			else
+			{
+				if (j != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(k + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
