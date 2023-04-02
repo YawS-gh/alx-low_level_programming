0b312417f0b9bf720e66cdad06734bc4c9fc061f
Yaw Sarkodie <yaw_sarkodie2003@yahoo.com>
2023-04-02 12:45:09 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - functions prints with proper grammer
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
